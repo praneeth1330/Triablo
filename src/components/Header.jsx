7373fa7203c0bea81a06dd3a297299bd850b08d0
Praneeth <103632161+praneeth1330@users.noreply.github.com>
2023-12-30 03:33:58 +0530

    Update Header.jsx
    fade removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
-import Fade from "react-reveal/Fade";
+
 
 const Header = () => {
 	return (
@@ -14,13 +14,13 @@ const Header = () => {
 				<div className="flex items-center justify-center h-screen">
 					<div className=" flex-col space-y-2 lg:space-y-5 align-middle text-center ">
 						<h2 className="text-2xl md:text-5xl">
-							<Fade bottom>THE STYLE FOR TODAY</Fade>
+							THE STYLE FOR TODAY
 						</h2>
 						<p className="text-sm text-center">
-							<Fade bottom>
+							
 								Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
 								Impedit, recusandae beatae?
-							</Fade>
+						
 						</p>
 						<button className="rounded-full px-2 py-1 lg:px-5  bg-[#e8846b] hover:bg-[#222] text-sm font-sans">
 							CHECK IT
