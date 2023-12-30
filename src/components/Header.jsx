35bbd2b95c7f611d4d62530683effeeb273b6f14
Praneeth <rockpraneeth33@gmail.com>
2023-12-30 08:13:56 +0530

    fade removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,10 @@ const Header = () => {
 			<div className="lg:w-8/12">
 				<div className="flex items-center justify-center h-screen">
 					<div className=" flex-col space-y-2 lg:space-y-5 align-middle text-center ">
-						<h2 className="text-2xl md:text-5xl">
-							<Fade bottom>THE STYLE FOR TODAY</Fade>
-						</h2>
+						<h2 className="text-2xl md:text-5xl">THE STYLE FOR TODAY</h2>
 						<p className="text-sm text-center">
-							<Fade bottom>
-								Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
-								Impedit, recusandae beatae?
-							</Fade>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
+							Impedit, recusandae beatae?
 						</p>
 						<button className="rounded-full px-2 py-1 lg:px-5  bg-[#e8846b] hover:bg-[#222] text-sm font-sans">
 							CHECK IT
